7c83106228620b009de4c6fbbc74189c92920520
girindra <gdwiixa@gmail.com>
2026-02-27 10:42:25 +0700

    install laravel ui package

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+// redirect ke halaman login
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
